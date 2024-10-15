3c987ab78c93e41dfb8373358d11a79f9d1adbf7
e1b22001 <e1b22001@oit.ac.jp>
2024-10-15 13:50:18 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team_8/db8/Db8Application.java
@@ -0,0 +1,13 @@
+package oit.is.team_8.db8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db8Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team_8/db8/Db8ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team_8.db8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
