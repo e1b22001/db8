038e147b83edfc0cb72ef21e6c033bcc9cf4024b
ok2258 <gomo20031119@gmail.com>
2024-10-15 15:53:11 +0900

    kansei

--- src/main/java/oit/is/team_8/db8/controller/Sample41Controller.java
+++ src/main/java/oit/is/team_8/db8/controller/Sample41Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import oit.is.team_8.db8.model.Chamber;
 import oit.is.team_8.db8.model.ChamberMapper;
 import oit.is.team_8.db8.model.ChamberUser;
-//import oit.is.team_8.db8.model.UserInfo;
+import oit.is.team_8.db8.model.UserInfo;
 
 @Controller
 @RequestMapping("/sample4")
@@ -75,6 +75,25 @@ public class Sample41Controller {
   @GetMapping("step7")
   @Transactional
   public String sample47(ModelMap model) {
+    ArrayList<ChamberUser> chamberUsers7 = chamberMapper.selectAllChamberUser();
+    model.addAttribute("chamberUsers7", chamberUsers7);
+    return "sample46.html";
+  }
+
+  @PostMapping("step8")
+  @Transactional
+  public String sample48(@RequestParam Double height, @RequestParam Integer age, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    UserInfo ui = new UserInfo();
+    ui.setUserName(loginUser);
+    ui.setAge(age);
+    ui.setHeight(height);
+    try {
+      chamberMapper.insertUserInfo(ui);
+    } catch (RuntimeException e) {// 既に身長が登録されているユーザでさらに登録しようとすると実行時例外が発生するので，コンソールに出力してinsertをSkipする
+      System.out.println("Exception:" + e.getMessage());
+    }
+    // insert後にすべての身長が登録されているユーザを取得する
     ArrayList<ChamberUser> chamberUsers7 = chamberMapper.selectAllChamberUser();
     model.addAttribute("chamberUsers7", chamberUsers7);
     return "sample46.html";
